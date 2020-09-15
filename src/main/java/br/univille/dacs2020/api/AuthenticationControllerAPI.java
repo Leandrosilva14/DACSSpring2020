4cce3e7d5de3c2173dd8398ca7488ba815503d15
LEANDRO MACHADO DA SILVA <leandrosilva.1@univille.br>
2020-09-15 23:11:38 +0000

    swagger configs

--- src/main/java/br/univille/dacs2020/api/AuthenticationControllerAPI.java
+++ src/main/java/br/univille/dacs2020/api/AuthenticationControllerAPI.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
  
 import br.univille.dacs2020.model.Usuario;
 import br.univille.dacs2020.security.JWTUtil;
